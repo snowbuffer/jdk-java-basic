6b72356bd2b23a1cac45c3d11488243776e149a4
junbo.chen <timmydargon@sina.com>
2021-05-12 16:10:50 +0800

    SynchronousQueue测试

--- thread-concurrent/src/test/java/com/wangwenjun/juc/collections/blocking/SynchronousQueueExampleTest.java
+++ thread-concurrent/src/test/java/com/wangwenjun/juc/collections/blocking/SynchronousQueueExampleTest.java
@@ -17,6 +17,14 @@ import static org.junit.Assert.fail;
  ***************************************/
 public class SynchronousQueueExampleTest {
 
+    /**
+     * SynchronousQueue ： 两个线程之前的同步控制， 本身没有容量的概念，一个元素存放到队列必须与一个元素取值同步发生才有效(即两个线程之间的同步交换数据)
+     * 生产者线程  put(数据1) -> 等待         -> put成功
+     * 消费者线程                     take               -> take结果：数据1
+     * 以上规则跟exchange很像
+     * 使用场景：观察者模式： 一个生产者发布事件，一个消费者负责及时响应事件
+     */
+
     @Test
     public void testAdd() throws InterruptedException {
         SynchronousQueue<String> queue = SynchronousQueueExample.create();
@@ -30,6 +38,7 @@ public class SynchronousQueueExampleTest {
         });
 
         TimeUnit.MILLISECONDS.sleep(5);
+        // add 不适合在SynchronousQueue使用
         assertThat(queue.add("SynchronousQueue"), equalTo(true));
     }
 
@@ -68,18 +77,16 @@ public class SynchronousQueueExampleTest {
     @Test
     public void testPut() throws InterruptedException {
         SynchronousQueue<String> queue = SynchronousQueueExample.create();
-/*
         Executors.newSingleThreadExecutor().submit(() -> {
             try {
+                TimeUnit.SECONDS.sleep(5);
                 assertThat(queue.take(), equalTo("SynchronousQueue"));
             } catch (Exception e) {
                 e.printStackTrace();
             }
         });
-
-        TimeUnit.MILLISECONDS.sleep(5);*/
-        queue.put("SynchronousQueue");
-        fail("should not process to here.l");
+        queue.put("SynchronousQueue"); // 添加元素，一直等到有消费者取值才返回
+        System.out.println("==");
     }
 
 }
