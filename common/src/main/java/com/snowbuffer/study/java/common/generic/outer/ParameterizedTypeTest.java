66892ce3d30f72998b5e83080e3db10619abedf5
junbo.chen <timmydargon@sina.com>
2021-05-27 11:34:45 +0800

    泛型测试

--- common/src/main/java/com/snowbuffer/study/java/common/generic/outer/ParameterizedTypeTest.java
+++ common/src/main/java/com/snowbuffer/study/java/common/generic/outer/ParameterizedTypeTest.java
@@ -1,70 +1,34 @@
 package com.snowbuffer.study.java.common.generic.outer;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import java.io.Serializable;
+import java.lang.reflect.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class ParameterizedTypeTest {
+public class ParameterizedTypeTest<T extends Number & Serializable, V> {
 
-    /**
-     * 1、map: 获取ParameterizedType:class java.lang.String
-     * 2、map: 获取ParameterizedType:class com.wangji.demo.ParameterizedTypeTest
-     * 3、map:getOwnerType is null
-     * 4、map:getRawType:interface java.util.Map
-     */
-    private Map<String, ParameterizedTypeTest> map;
-    /**
-     * 1、set1: 获取ParameterizedType:class java.lang.String
-     * 2、set1:getOwnerType is null
-     * 3、set1:getRawType:interface java.util.Set
-     */
+    private Map<String, ParameterizedTypeTest> map;  // 存在泛型，则整个变量就是一个ParameterizedType
     private Set<String> set1;
-    /**
-     * 1、 clz: 获取ParameterizedType:?
-     * 2、 clz:getOwnerType is null
-     * 3、clz:getRawType:class java.lang.Class
-     */
     private Class<?> clz;
-    /**
-     * 1、holder: 获取ParameterizedType:class java.lang.String
-     * 2、holder:getOwnerType:class com.wangji.demo.ParameterizedTypeTest
-     * 3、holder:getRawType:class com.wangji.demo.ParameterizedTypeTest$Holder
-     */
+    private Class<T> tclz;
+    private Class<List<T>> tclzList;
+    private Class<T[]> tclzArray;
+    private Class<String[]> stringclzArray;
     private Holder<String> holder;
-
-    /**
-     * 1、list: 获取ParameterizedType:class java.lang.String
-     * 2、list:getOwnerType is null
-     * 3、list:getRawType:interface java.util.List
-     */
     private List<String> list;
-    /**
-     * str:is not ParameterizedType
-     */
-    private String str;
-    /**
-     * i:is not ParameterizedType
-     */
+    private String str; // 非ParameterizedType
     private Integer i;
-    /**
-     * set:is not ParameterizedType
-     */
     private Set set;
-    /**
-     * aList:is not ParameterizedType
-     */
     private List aList;
-    /**
-     * 1、entry: 获取ParameterizedType:class java.lang.String
-     * 2、entry: 获取ParameterizedType:class java.lang.String
-     * 3、entry:getOwnerType:interface java.util.Map
-     * 4、entry:getRawType:interface java.util.Map$Entry
-     */
     private Map.Entry<String, String> entry;
-
+    private List<? extends Number> a;
+    private List<? super String> b;
+    private T key;
+    private V value;
+    private V[] values;
+    private List<T> tList;
+    private Map<String, List<T>> map2;
 
     static class Holder<V> {
     }
@@ -78,32 +42,108 @@ public class ParameterizedTypeTest {
                 if (f.getName().equals("log")) {
                     continue;
                 }
-                if (f.getGenericType() instanceof ParameterizedType) {
-                    ParameterizedType parameterizedType = (ParameterizedType) f.getGenericType();
-                    for (Type type : parameterizedType.getActualTypeArguments()) {
-                        System.out.println(f.getName() + ": 获取ParameterizedType:" + type);
-                    }
-                    if (parameterizedType.getOwnerType() != null) {
-                        System.out.println(f.getName() + ":getOwnerType:" + parameterizedType.getOwnerType());
-                    } else {
-                        System.out.println(f.getName() + ":getOwnerType is null");
-                    }
-                    if (parameterizedType.getRawType() != null) {
-                        System.out.println(f.getName() + ":getRawType:" + parameterizedType.getRawType());
-                    }
-                } else {
-                    System.out.println(f.getName() + ":is not ParameterizedType ");
-                }
+                System.out.println("[" + f.getName() + "]: \n\n\t\t 不带泛型信息的ClassType: " + f.getType());
+                printGenericInfo(f.getGenericType(), "\n\t\t");
+                System.out.println();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private static void printGenericInfo(Type targetType, String blank) {
+        if (targetType instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) targetType;
+            System.out.println(blank + " genericType:" + parameterizedType.getClass());
+            System.out.println(blank + " genericTypeString:" + parameterizedType);
+            System.out.println(blank + " ActualTypeArguments:[");
+            for (Type type : parameterizedType.getActualTypeArguments()) {
+                System.out.println(blank + "\t\t ActualTypeArgumentsType:" + judgeType(type));
+                if (!(type instanceof Class)) {
+                    printGenericInfo(type, blank + "\t\t\t\t");
+                }
+            }
+            System.out.println(blank + " ]");
+            if (parameterizedType.getOwnerType() != null) {
+                System.out.println(blank + " ownerType:" + judgeType(parameterizedType.getOwnerType()));
+            } else {
+                System.out.println(blank + " ownerType is null");
+            }
+            if (parameterizedType.getRawType() != null) {
+                System.out.println(blank + " rawType:" + judgeType(parameterizedType.getRawType()));
+            }
+        } else if (targetType instanceof TypeVariable) {
+            TypeVariable typeVariable = (TypeVariable) targetType;
+            System.out.println(blank + " typeVariableGenericDeclaration:" + typeVariable.getGenericDeclaration());
+            System.out.println(blank + " typeVariableName:" + typeVariable.getName());
+            if (typeVariable.getBounds().length == 0) {
+                System.out.println(blank + "\t\t typeVariableBounds: 不存在");
+            } else {
+                for (Type type : typeVariable.getBounds()) {
+                    System.out.println(blank + "\t\t typeVariableBoundName:" + type.getTypeName());
+                    System.out.println(blank + "\t\t typeVariableBoundType:" + judgeType(type));
+                }
+            }
+            if (typeVariable.getAnnotatedBounds().length == 0) {
+                System.out.println(blank + "\t\t typeVariableAnnotatedBounds: 不存在");
+            } else {
+                for (AnnotatedType annotatedBound : typeVariable.getAnnotatedBounds()) {
+                    System.out.println(blank + "\t\t typeVariableAnnotatedBound:" + annotatedBound);
+                    System.out.println(blank + "\t\t typeVariableAnnotatedBoundType:" + annotatedBound.getType().getTypeName());
+                }
+            }
+        } else if (targetType instanceof WildcardType) {
+            WildcardType wildcardType = (WildcardType) targetType;
+            if (wildcardType.getUpperBounds().length == 0) {
+                System.out.println(blank + "\t\t wildcardTypeUpperBoundType: 不存在");
+            } else {
+                for (Type type : wildcardType.getUpperBounds()) {
+                    System.out.println(blank + "\t\t wildcardTypeUpperBoundType:" + judgeType(type));
+                }
+            }
+            if (wildcardType.getLowerBounds().length == 0) {
+                System.out.println(blank + "\t\t wildcardTypeLowerBoundType: 不存在");
+            } else {
+                for (Type type : wildcardType.getLowerBounds()) {
+                    System.out.println(blank + "\t\t wildcardTypeLowerBoundType:" + judgeType(type));
+                }
+            }
+        } else if (targetType instanceof GenericArrayType) {
+            GenericArrayType genericArrayType = (GenericArrayType) targetType;
+            System.out.println(blank + "\t\t genericArrayTypeGenericComponentType:" + judgeType(genericArrayType.getGenericComponentType()));
+            if (!(genericArrayType.getGenericComponentType() instanceof Class)) {
+                printGenericInfo(genericArrayType.getGenericComponentType(), blank + "\t\t\t\t");
+            }
+        } else if (targetType instanceof Class) {
+            System.out.println(blank + " class:" + targetType);
+        }
+
+    }
+
+    private static String judgeType(Type type) {
+        if (type == null) {
+            return "是一个Null" + "[" + type + "]";
+        }
+        if (type instanceof Class) {
+            return "是一个Class" + "[" + type + "]";
+        }
+        if (type instanceof ParameterizedType) {
+            return "是一个ParameterizedType" + "[" + type + "]";
+        }
+        if (type instanceof TypeVariable) {
+            return "是一个TypeVariable" + "[" + type + "]";
+        }
+        if (type instanceof WildcardType) {
+            return "是一个WildcardType" + "[" + type + "]";
+        }
+        if (type instanceof GenericArrayType) {
+            return "是一个GenericArrayType" + "[" + type + "]";
+        }
+        return "未知" + "[" + type + "]";
+    }
 
     public static void main(String[] args) {
         testParameterizedType();
     }
-
 
 }
